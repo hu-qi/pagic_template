15162de47a01a741f1787c0263b990a79c726cd1
huqi <huqi1008301@163.com>
2021-02-04 10:01:55 +0800

    feat✨  update config

--- pagic.config.ts
+++ pagic.config.ts
@@ -1,1 +1,17 @@
-export default {};
+export default {
+    theme: 'blog',
+    plugins: ['blog'],
+    title: 'pagic template',
+    description: 'Use this template to create a Pagic site with the blog theme.',
+    github: 'https://github.com/hu-qi/pagic_template',
+    blog: {
+        root: '/posts/',
+        social: {
+          github: 'hu-qi/pagic_template',
+          email: 'huqi@gpdi.com',
+          twitter: 'huqii',
+          v2ex: 'huqi',
+          zhihu: 'fashaoge'
+        }
+      }
+};
